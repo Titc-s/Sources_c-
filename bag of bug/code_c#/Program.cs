7c8f6c3732c192ac290fa606cd461b1fe0ac7761
Titc <1240104681@qq.com>
2021-11-16 17:06:07 +0800

    qwq

---
+++ bag of bug/code_c#/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace code_c_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            test01 te = new test01();
+            string str = te.Hello();
+            Console.WriteLine("Hello World!" + str);
+        }
+    }
+}
---
+++ bag of bug/code_c#/test01.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace code_c_
+{
+
+
+    public class test01
+    {
+        public string Hello()
+        {
+            return "fine";
+        }
+    }
+}
