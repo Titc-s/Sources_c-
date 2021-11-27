5a42338b424b09b07129967ca38d653f75ce95bf
Titc <1240104681@qq.com>
2021-11-27 09:51:40 +0800

    yeah~

--- LeetCode/13.roman-to-integer.java
+++ LeetCode/13.roman-to-integer.java
@@ -12,34 +12,37 @@ class Solution {
         for (int i = 1; i < s.length(); i++) {
             int num = getValue(s.charAt(i));
             if (num_l < num)
-                sum -= sum;
+                sum -= num_l;
             else
-                sum += sum;
+                sum += num_l;
             num_l = num;
         }
+        int last = getValue(s.charAt(s.length() - 1));
+        sum = sum + last;
         return sum;
 
     }
 
     private int getValue(char Roman_way) {
         switch (Roman_way) {
-        case 'I':
-            return 1;
-        case 'V':
-            return 5;
-        case 'X':
-            return 10;
-        case 'L':
-            return 50;
-        case 'C':
-            return 100;
-        case 'D':
-            return 500;
-        case 'M':
-            return 1000;
-        default:
-            return 0;
+            case 'I':
+                return 1;
+            case 'V':
+                return 5;
+            case 'X':
+                return 10;
+            case 'L':
+                return 50;
+            case 'C':
+                return 100;
+            case 'D':
+                return 500;
+            case 'M':
+                return 1000;
+            default:
+                return 0;
         }
     }
 }
+
 // @lc code=end
