53421a00707b6c9ce833a2de2d9d3fdb3b392b53
Titc <1240104681@qq.com>
2021-11-27 09:42:12 +0800

    road to Roman

---
+++ LeetCode/13.roman-to-integer.java
@@ -0,0 +1,45 @@
+/*
+ * @lc app=leetcode id=13 lang=java
+ *
+ * [13] Roman to Integer
+ */
+
+// @lc code=start
+class Solution {
+    public int romanToInt(String s) {
+        int sum = 0;
+        int num_l = getValue(s.charAt(0));
+        for (int i = 1; i < s.length(); i++) {
+            int num = getValue(s.charAt(i));
+            if (num_l < num)
+                sum -= sum;
+            else
+                sum += sum;
+            num_l = num;
+        }
+        return sum;
+
+    }
+
+    private int getValue(char Roman_way) {
+        switch (Roman_way) {
+        case 'I':
+            return 1;
+        case 'V':
+            return 5;
+        case 'X':
+            return 10;
+        case 'L':
+            return 50;
+        case 'C':
+            return 100;
+        case 'D':
+            return 500;
+        case 'M':
+            return 1000;
+        default:
+            return 0;
+        }
+    }
+}
+// @lc code=end
